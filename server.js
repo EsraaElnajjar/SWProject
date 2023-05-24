fa8632df71e0e698cab24c6e257efc96d0c33aa0
EsraaAhmedAli <117761521+EsraaAhmedAli@users.noreply.github.com>
2023-05-24 11:24:44 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-console.log("connect")
+import express from "express";
+import { engine } from 'express-handlebars';
+import methodoverride from 'method-override';
+import dotenv from 'dotenv';
+dotenv.config();
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+
+mongoose.connect(process.env.mongoconnectionurl)
+import subjectsRouter from './routes/subjects.js';
+import authroute from './routes/auth.js';
+import {authentcation} from './middleware/authentcation.js';
+const app = express();
+app.use(cookieParser())
+app.use(express.urlencoded({extended: true}))
+app.use(methodoverride('_method'))
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './templates');
+app.use('/',authroute)
+
+app.use('/subjects',authentcation,subjectsRouter);
+
+
+app.listen(process.env.port,()=>{
+    console.log('start the app on http://Localhost '+process.env.port);
+});
